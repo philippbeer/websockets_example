b3894fc9ad166e571d17f60b3dfac3e794fa232b
Philipp Beer <philipp@sciscry.ai>
2021-06-02 10:49:18 +0200

    sending data from browser to server

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ const wss = new WebSocket.Server({ port: 8082});
 wss.on("connection", ws => {
     console.log("client connected!");
 
+    ws.on("message", data => {
+	console.log(`Client has sent us: ${data}`);
+    });
 
     ws.on("close", () => {
 	console.log("Client has disconnected");
