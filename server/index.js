61c5ae5609f4490f72712d2909d1824bbfa3e29b
Philipp Beer <philipp@sciscry.ai>
2021-06-02 11:43:23 +0200

    add json to web socket transfer

--- server/index.js
+++ server/index.js
@@ -3,15 +3,12 @@ const WebSocket = require("ws");
 const wss = new WebSocket.Server({ port: 8082});
 
 wss.on("connection", ws => {
-    console.log("client connected!");
-
-    ws.on("message", data => {
-	console.log(`Client has sent us: ${data}`);
-
-	ws.send("message received");
-    });
-
-    ws.on("close", () => {
-	console.log("Client has disconnected");
+    ws.on("message", message => {
+	try {
+	    const data = JSON.parse(message);
+	    console.log(data.lat, data.lon);
+	} catch(e) {
+	    console.log(`Error occurred: ${e.message}`);
+	}
     });
 });
